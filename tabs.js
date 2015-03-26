6d55f7bed8cc5fca033bb5fe45eac96ba9990445
App Academy Student <student@appacademy.io>
2015-03-26 12:01:46 -0700

    first commit

---
+++ tabs.js
@@ -0,0 +1,31 @@
+
+$.Tabs = function(el) {
+  this.$el = $(el);
+  this.$contentTabs = $(this.$el.attr('data-content-tabs'));
+  this.$activeTab = this.$contentTabs.find('.active');
+  this.setupEventHandler();
+};
+
+$.Tabs.prototype.setupEventHandler = function() {
+  this.$el.on('click','a', function(event) {
+    event.preventDefault();
+    this.clickTab($(event.currentTarget));
+  }.bind(this));
+
+};
+
+$.Tabs.prototype.clickTab = function($a) {
+  //set active class to only clicked link
+  this.$el.find('a').removeClass("active");
+  $a.addClass('active');
+
+  //set transition
+  this.$activeTab.removeClass('active');
+  this.$activeTab.addClass('transitioning');
+  this.$contentTabs.one('transitionend', '.tab-pane', function(event) {
+    $(event.currentTarget).removeClass('transitioning');
+    this.$activeTab.addClass('active');
+  }.bind(this));
+
+  this.$activeTab = this.$contentTabs.find($a.attr('href'));
+};
